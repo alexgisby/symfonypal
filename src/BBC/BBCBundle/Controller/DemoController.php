4761b87ed9fac9648cf0b68bfb2fb2c42269bd76
miguelgonz <Miguel.Gonzlez@bbc.co.uk>
2013-09-20 14:49:49 +0100

    Style

--- src/BBC/BBCBundle/Controller/DemoController.php
+++ src/BBC/BBCBundle/Controller/DemoController.php
@@ -17,21 +17,31 @@ class DemoController extends BaseController
     {
         $rawservices = ServiceModel::fetchServices();
         $tv_services = $radio_services = array();
+        $regions = array();
         foreach ($rawservices as $s) {
             $obj = array(
                 'id'=>(string)$s->sid,
                 'type'=>(string)$s->type,
                 'name'=>(string)$s->name,
             );
+            $obj['description'] = (isset($s->description))?(string)$s->description:'No description';
+
             if ($obj['type'] === 'TV') {
                 $tv_services[] = $obj;
             } else {
                 $radio_services[] = $obj;
             }
+            if (isset($s->region)) {
+                $regions[ucfirst((string)$s->region)] = true;
+            }
         }
+        $regions = array_keys($regions);
+        sort($regions);
+
         $viewData = $this->getBarlesque();
         $viewData['radio_services'] = $radio_services;
         $viewData['tv_services'] = $tv_services;
+        $viewData['regions'] = $regions;
         return $this->render('BBCBundle:Demo:index.html.twig', $viewData);
     }
 
