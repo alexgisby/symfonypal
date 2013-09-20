53a41657a56c955f763e5e8bbe64b1371de5eb5b
Alex Gisby <alex@solution10.com>
2013-09-20 11:10:02 +0100

    BBC Bundle added

---
+++ src/BBC/BBCBundle/BBCBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BBC\BBCBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BBCBundle extends Bundle
+{
+}
---
+++ src/BBC/BBCBundle/Controller/WelcomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BBC\BBCBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class WelcomeController extends Controller
+{
+    public function indexAction()
+    {
+        /*
+         * The action's view can be rendered using render() method
+         * or @Template annotation as demonstrated in DemoController.
+         *
+         */
+        return $this->render('BBCBundle:Welcome:index.html.twig');
+    }
+}
